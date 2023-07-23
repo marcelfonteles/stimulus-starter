604e50e6e5d345086cbb3fbd7d8510fd4d7bfc4c
marcelfonteles <marcelfonteles@gmail.com>
2023-07-23 18:40:59 +0000

    Hello, Stimulus

---
+++ src/controllers/hello_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = [ 'username' ];
+
+    connect() {
+        console.log('Hello, Stimulus!', this.element);
+    }
+
+    greet() {
+        // const element = this.usernameTarget;
+        // const name = element.value;
+        
+        console.log(`Hello, ${this.username}!`);
+        
+        // console.log('Hello, Greet', this.element);
+    }
+
+    get username() {
+        return this.usernameTarget.value;
+    }
+
+    keyPress(e) {
+        if (e.keyCode === 13) {
+            this.greet();
+        }
+    }
+}
+
+// controllers
+// data-controller='controller-name'
+// data-controller-name-target='username'
+// data-action='event->controller-name#classMethod'
